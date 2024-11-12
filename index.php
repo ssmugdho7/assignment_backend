5695582ddd47df4b2cfa1be1a440ccb1bbc1dec1
ssmugdho7 <145373831+ssmugdho7@users.noreply.github.com>
2024-11-12 16:22:44 +0600

    No need to add style as bootstrap handles the column spanning by col class

--- index.php
+++ index.php
@@ -4,15 +4,7 @@
 <head>
 
 
-    <style>
-    .form-group>.row1 {
-        grid-column: 1/span2;
-    }
-
-    .form-group>.row2 {
-        grid-column: 1 / span2;
-    }
-    </style>
+  
     <title>User Registration</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
