96bbb3f17bbda3887f6a142909f19f38ab0f4f39
ssmugdho7 <145373831+ssmugdho7@users.noreply.github.com>
2024-11-12 18:06:48 +0600

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,18 +1,18 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Replace with your DB username
-$password = "";      // Replace with your DB password
+$username = "root";  
+$password = "";     
 $dbname = "ecommerce";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Collect form data
+
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $address_line1 = $_POST['address_line1'];
@@ -35,7 +35,7 @@ $ref2_last_name = $_POST['ref2_last_name'];
 $ref2_email = $_POST['ref2_email'];
 $ref2_phone = $_POST['ref2_phone'];
 
-// Insert data into the database
+
 $sql = "INSERT INTO registration (first_name, last_name, address_line1, address_line2, city, region, postal_code, country, email, phone, found_us, feedback, recommend_us, ref1_first_name, ref1_last_name, ref1_email, ref1_phone, ref2_first_name, ref2_last_name, ref2_email, ref2_phone)
 VALUES ('$first_name', '$last_name', '$address_line1', '$address_line2', '$city', '$region', '$postal_code', '$country', '$email', '$phone', '$found_us', '$feedback', '$recommend_us', '$ref1_first_name', '$ref1_last_name', '$ref1_email', '$ref1_phone', '$ref2_first_name', '$ref2_last_name', '$ref2_email', '$ref2_phone')";
 
